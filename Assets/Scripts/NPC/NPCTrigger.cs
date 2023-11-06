300f074f1d84ebaf61384948ce650d6895029c10
Dare-Mensah <91201253+Dare-Mensah@users.noreply.github.com>
2023-11-06 07:54:38 +0000

    Update NPCTrigger.cs

--- Assets/Scripts/NPC/NPCTrigger.cs
+++ Assets/Scripts/NPC/NPCTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-public class NPCTrigger : MonoBehaviour
+public class NPCTrigger : MonoBehaviour //SCRIPT IS CURRENTLY NOT IN  USE WILL BE DELETED I  FURTURE ITERATIONS
 {
 
     public GameObject greetingText;
