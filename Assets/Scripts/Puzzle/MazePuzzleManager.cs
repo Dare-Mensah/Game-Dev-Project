3106e7178d5abcb3dd33b5844448fe17b4fdf4da
Dare-Mensah <91201253+Dare-Mensah@users.noreply.github.com>
2023-12-23 22:47:05 +0000

    game update 5

--- Assets/Scripts/Puzzle/MazePuzzleManager.cs
+++ Assets/Scripts/Puzzle/MazePuzzleManager.cs
@@ -8,12 +8,25 @@ public class MazePuzzleManager : MonoBehaviour
     public GameObject puzzleCam;
     public GameObject Ball;
 
+    public GameObject rocks;
+    public GameObject winText;
+    public GameObject loseText;
     // Update is called once per frame
     void Update()
     {
         if(puzzleCam.activeInHierarchy == true) //checks if the puzzleCam is active
         {
             Ball.SetActive(true); //sets teh abll to true
+            rocks.SetActive(true);
+        }
+
+        if(winText.activeInHierarchy ==true)
+        {
+            rocks.SetActive(false);
+        }
+        else if(loseText.activeInHierarchy ==true)
+        {
+            rocks.SetActive(false);
         }
     }
 }
