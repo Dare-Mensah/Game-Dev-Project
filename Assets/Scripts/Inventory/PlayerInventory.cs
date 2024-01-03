3041a16a99f0da9734c7cdc0a503ea53bb321386
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-03 18:20:30 +0000

    Update PlayerInventory.cs

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -1,22 +1,22 @@
-using System.Collections;
+using System.Collections;// use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;//importing unityengine to allow for unity based work to be conducted
 using TMPro;
 
 public class PlayerInventory : MonoBehaviour
-{
+{ //declaring the playerinventory class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
 
-    public int Gold;
-    public TextMeshProUGUI GoldAmountText;
+    public int Gold; //the use of a public variable helps to keep a track and record of the players gold quantity
+    public TextMeshProUGUI GoldAmountText; //public variable is used to thus display this quantity of gold the player has within the UI
     // Start is called before the first frame update
     void Start()
     {
-        
+        //nothing happens in this instance as the empty start method is used for intialisation
     }
 
     // Update is called once per frame
     void Update()
     {
-        GoldAmountText.text = Gold + " Gold";
+        GoldAmountText.text = Gold + " Gold"; //this helps to ensure that the User interface features/elements are updated to reflect current gold quantites
     }
 }
