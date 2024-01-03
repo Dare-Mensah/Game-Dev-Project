27125776b7788bb2ba5eb3e7c43e8eb6dc4efc9c
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-03 22:27:41 +0000

    Update ThirdPersonCamera.cs

--- Assets/Scripts/Player/ThirdPersonCamera.cs
+++ Assets/Scripts/Player/ThirdPersonCamera.cs
@@ -1,29 +1,30 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
-{
+{// thirdpersoncamera class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
 
     [Header("Refernces")]
     //Refernces to the in game transform objects.
-    public Transform Orientation;
-    public Transform Player;
-    public Transform PlayerObject;
-    public Rigidbody rB;
+    public Transform Orientation; //this references an orientation transform that controls the camera orientation
+    public Transform Player;// public var to reference a players transformation
+    public Transform PlayerObject; // this is a public transform that references the playerobject
+    public Rigidbody rB; //references the rigidbody assigned to playable character
 
-    public float rotatioSpeed;
+    public float rotatioSpeed; //float variable dictates the speed of camera rotation
 
-    public Transform focusLookAt;
+    public Transform focusLookAt; // transform is utilised to focus camera style
 
-    public GameObject BasicCam;
+    public GameObject BasicCam; //references the basic camera game object
     //public GameObject FocusCam;
     //public GameObject PuzzleCam;
 
-    public CamraPosition currentstyle;
+    public CamraPosition currentstyle;//variable tracks the current camera dynamic
 
     public enum CamraPosition
-    {
+    { //this is utilised to define a variety of different camera styles
         Basic, 
         Focus,
         Puzzle,
@@ -50,26 +51,26 @@ public class ThirdPersonCamera : MonoBehaviour
 
 
         //finding out the rotation oritnation
-        Vector3 viewDirection = Player.position - new Vector3(transform.position.x, Player.position.y, transform.position.z);
-        Orientation.forward = viewDirection.normalized;
+        Vector3 viewDirection = Player.position - new Vector3(transform.position.x, Player.position.y, transform.position.z);//calculating the direction of camera in relation to that of the player
+        Orientation.forward = viewDirection.normalized; //this sets the orientation to be a forward directionin comparison to view direction
 
         //Rotating the player object
         if(currentstyle == CamraPosition.Basic || currentstyle == CamraPosition.Puzzle) // normal basic camera angle
         {
-            float horizontalInput = Input.GetAxis("Horizontal"); //horizontal input
-            float verticalInput = Input.GetAxis("Vertical"); //vertical input
+            float horizontalInput = Input.GetAxis("Horizontal"); //code obtains the horizontal input
+            float verticalInput = Input.GetAxis("Vertical"); //code obtains the vertical input
             Vector3 inputDirection = Orientation.forward * verticalInput + Orientation.right * horizontalInput;
 
-            if (inputDirection != Vector3.zero)
+            if (inputDirection != Vector3.zero) //if condition checks to see if any input is detected at which point it will redirect player direction to the input direction
                 PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, inputDirection.normalized, Time.deltaTime * rotatioSpeed);
         }
         
         else if(currentstyle == CamraPosition.Focus)
         {
-            Vector3 dirToFocusLookAt = focusLookAt.position - new Vector3(transform.position.x, focusLookAt.position.y, transform.position.z);
+            Vector3 dirToFocusLookAt = focusLookAt.position - new Vector3(transform.position.x, focusLookAt.position.y, transform.position.z);  
             Orientation.forward = dirToFocusLookAt.normalized; // focusing camera on the focus empty game object to provide a new camera angle 
 
-            PlayerObject.forward = dirToFocusLookAt.normalized;
+            PlayerObject.forward = dirToFocusLookAt.normalized; //this sets the players forward direction to be that of the the direction of the focus look at Transform
         }
     }
 
@@ -84,7 +85,7 @@ public class ThirdPersonCamera : MonoBehaviour
         //if (newPosition == CamraPosition.Focus) FocusCam.SetActive(true);
         //if (newPosition == CamraPosition.Puzzle) PuzzleCam.SetActive(true);
 
-        currentstyle = newPosition;
+        currentstyle = newPosition; //updating the current style to be representative of the nee position as shown through use of intialisation
 
     }
 }
