52c2a79783e70c12756800f8739deb1769a36783
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-03 20:11:23 +0000

    Update ChangeCam.cs

--- Assets/Scripts/Platform/ChangeCam.cs
+++ Assets/Scripts/Platform/ChangeCam.cs
@@ -1,28 +1,28 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unityengine to allow for unity based work to be conducted
 
 public class ChangeCam : MonoBehaviour
-{
-    public GameObject triggerCam;
+{ // playerlookat class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+    public GameObject triggerCam; //public gameobject that reference to the camera
 
-    public AudioSource bridgeSound;
+    public AudioSource bridgeSound; //public audiosource is a reference to sound that will be played when camera pans
 
 
     private void OnTriggerEnter(Collider other)
-    {
+    { //check to see if collider that is entered is tagged as "player"
         if(other.gameObject.tag == "Player") //if the player enters the trigger then the trigger cam is set to active
         {
-            triggerCam.SetActive(true);
-            bridgeSound.Play();
+            triggerCam.SetActive(true); //this activates the trigger camera that changes player viewpoint
+            bridgeSound.Play(); //this plays bridgesound resultant of camera change
         }
     }
 
     private void OnTriggerExit(Collider other)
-    {
+    { // check to see if the collider that exited is tagged as "player"
         if (other.gameObject.tag == "Player") //if the player enters the trigger then the trigger cam is set to false
         {
-            triggerCam.SetActive(false); 
+            triggerCam.SetActive(false);  //the trigger camera is deactivated reverting to default camera setting
 
         }
     }
