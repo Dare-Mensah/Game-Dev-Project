c4d034b9690d3dd35ff8076d515e595f22446c19
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-03 19:52:35 +0000

    Update PlayerLookAt.cs

--- Assets/Scripts/NPC/PlayerLookAt.cs
+++ Assets/Scripts/NPC/PlayerLookAt.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLookAt : MonoBehaviour
-{
+{// playerlookat class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+    public TextMeshProUGUI textComponent; 
     // Start is called before the first frame update
     public Transform Player;
 
