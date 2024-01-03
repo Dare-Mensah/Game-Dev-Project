7bfa31c3a1b0f304bc423b7d1225eb8b354514ed
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-03 20:36:03 +0000

    Update WayPointPath.cs

--- Assets/Scripts/Platform/WayPointPath.cs
+++ Assets/Scripts/Platform/WayPointPath.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unityengine to allow for unity based work to be conducted
 
 public class WayPointPath : MonoBehaviour
-{
+{// playerlookat class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +17,21 @@ public class WayPointPath : MonoBehaviour
         
     }
 
-    public Transform GetWayPoint(int wayPointIndex)
+    public Transform GetWayPoint(int wayPointIndex) //public method for attaining the waypoints transform which is determined based on its index
     {
-        return transform.GetChild(wayPointIndex);//returns the transform of the way point idex
+        return transform.GetChild(wayPointIndex);//returns the transform of the way point index
     }
 
     public int getNextWayPointIndex(int currentWayPointIndex)
-    {
-        int nextWayPointIndex = currentWayPointIndex + 1;
+    { // public method is used to calculate the index of the proceeding way point
+        int nextWayPointIndex = currentWayPointIndex + 1; // increases the current waypoint index to obtain the next waypoint index
 
         if (nextWayPointIndex == transform.childCount)
-        {
+        { //if condition checks to see if the next way point index equals the no of children 
             nextWayPointIndex = 0; //sets the index to zero to start from the begining.
         }
 
-        return nextWayPointIndex;
+        return nextWayPointIndex; //return statement used in order to return the ascertained next way point index
     }
 
 }
