b3c41ecdc5964beb0aa07c071b78ccd377e158fc
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:18:31 +0000

    Update PlayerController.cs

--- Assets/Scripts/Puzzle 3/PlayerController.cs
+++ Assets/Scripts/Puzzle 3/PlayerController.cs
@@ -1,6 +1,6 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +9,12 @@ public class PlayerController : MonoBehaviour
     // Called 60 times a second.
     // Similar to the Update function, but used for physics calculations.
     void FixedUpdate()
-    {
+    { //method is called in fixed intervals for more effective process handlings
         // Get the horizontal input.
         // 0 = nothing
         // 1 = right
         // -1 = left
-        float xInput = Input.GetAxis("Horizontal");
+        float xInput = Input.GetAxis("Horizontal");//allows for input to be detected from user action i.e how the player intends to move across horizontal axis
         // Add force based on our input.
         rig.AddForce(Vector3.right * xInput * moveForce);
     }
