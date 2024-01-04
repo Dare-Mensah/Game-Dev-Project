abf565f29494a6700089ddbeda25038d34fc959f
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:25:12 +0000

    Update Coin.cs

--- Assets/Scripts/Puzzle 4/Coin.cs
+++ Assets/Scripts/Puzzle 4/Coin.cs
@@ -1,15 +1,16 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 
 public class Coin : MonoBehaviour
-{
+{ // the coin class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
     // Start is called before the first frame update
 
     private void OnCollisionEnter(Collision collision)
-    {
+    { //method is called if the player enters the collision collider and thus proceeding code is actioned upon
         if (collision.gameObject.tag == "Ball")
-        {
+        { //if statement is a conditional check to see whether the game object is tagged as being "ball"
             Destroy(gameObject); //DESTROYS THE CUBE IF IT COLLDIES WITH THE BALL
 
         }
