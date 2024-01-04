f419d73facacc20bb99b680335422765c8a92541
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:37:50 +0000

    Update MoveCamera.cs

--- Assets/Scripts/Puzzle 5/MoveCamera.cs
+++ Assets/Scripts/Puzzle 5/MoveCamera.cs
@@ -1,13 +1,14 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 
 public class MoveCamera : MonoBehaviour
-{
-    public Transform cameraPosition;
+{  // the movecamera class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
+    public Transform cameraPosition; //declaring the camera position of which is modified within unity to follow the game object desired
 
     private void Update()
-    {
-        transform.position = cameraPosition.position;
+    { //defining private method update that is called once a frame
+        transform.position = cameraPosition.position; //sets the position of the current object to the position of the camera object
     }
 }
