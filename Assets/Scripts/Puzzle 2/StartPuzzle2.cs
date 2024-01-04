080a241990b217f57543fddbae3ec7d59487dd63
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:15:36 +0000

    Update StartPuzzle2.cs

--- Assets/Scripts/Puzzle 2/StartPuzzle2.cs
+++ Assets/Scripts/Puzzle 2/StartPuzzle2.cs
@@ -1,15 +1,16 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 
 public class StartPuzzle2 : MonoBehaviour
-{
+{  // the startpuzzle2 class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
     //the camera inside the puzzle
-    public GameObject puzzleCam;
+    public GameObject puzzleCam; //public reference made to the game object puzzle cam
     //primary camera attached to the player
-    public GameObject playerCam;
+    public GameObject playerCam; //public reference made to the game object player camera
     //player who is moving
-    public GameObject player;
+    public GameObject player; //public reference to the game object player
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +24,14 @@ public class StartPuzzle2 : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    { //method is called as the player enters the trigger collider
         //check E bottom is pressed
         if(Input.GetKeyDown(KeyCode.E))
-        {
+        { //condition based check to see if input corresponds to the key "E" which invokes proceeding code
             //make puzzle camera,and player in minigame on, rest turns off, to prevent player in big map is affect by control.
-            puzzleCam.SetActive(true);
-            player.SetActive(false);
-            playerCam.SetActive(false);
+            puzzleCam.SetActive(true);//setActive() method is used to set the puzzlecam to false 
+            player.SetActive(false); //setActive() method is used to set the player to false 
+            playerCam.SetActive(false); //setActive() method is used to set the playercam to false 
         }
     }
     //this game is inspired by video https://www.youtube.com/watch?v=gkBntY22Oco
