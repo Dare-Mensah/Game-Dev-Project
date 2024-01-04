764a791764033bc239c239f80cefd784d0385f32
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:31:28 +0000

    Update MovementRunner.cs

--- Assets/Scripts/Puzzle 4/MovementRunner.cs
+++ Assets/Scripts/Puzzle 4/MovementRunner.cs
@@ -1,25 +1,26 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 using TMPro;
 using UnityEngine.UI;
 
 public class MovementRunner : MonoBehaviour
-{
-    public float moveSpeed = 3;
+{  // the coin class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
 
-    public float left_right_speed = 4;
+    public float moveSpeed = 3; //public reference and declaration to the move speed variable which is intialised to a value of 3 secs
+
+    public float left_right_speed = 4; //public reference and declaration of the left right speed which is initialised to a value of 4 secs
 
 
     [SerializeField] TextMeshProUGUI _score;
     //present target value on UI
     [SerializeField] TextMeshProUGUI _target;
     //track player score
-    public int score = 0;
+    public int score = 0; //public reference to the score variable which is initialised to the value of 0
     //value that player acheived
-    public int target;
+    public int target; //public reference to the variable target
     //message to output if player is winning or not
-    public GameObject WinText;
+    public GameObject WinText; //public reference to the game object win text
 
 
     // Update is called once per frame
@@ -27,25 +28,25 @@ public class MovementRunner : MonoBehaviour
     {
         transform.Translate(Vector3.right * Time.deltaTime * moveSpeed); //moving the player forward
 
-        if(Input.GetKey(KeyCode.A)) //checks if user inputs A
+        if(Input.GetKey(KeyCode.A)) //checks if user inputs A as per the getkey function that is utilised here
         { 
             transform.Translate(Vector3.forward * Time.deltaTime * left_right_speed); //moving charcater left
 
         }
-        if (Input.GetKey(KeyCode.D))  //checks if user inputs D
+        if (Input.GetKey(KeyCode.D))  //checks if user inputs the character D which is denoted by the key code and obtained by the getkey function
         {
             transform.Translate(Vector3.back * Time.deltaTime * left_right_speed); //moving charcater right
         }
 
 
-        _score.text = score.ToString("00");
-        _target.text = target.ToString("00");
+        _score.text = score.ToString("00"); //appends the text value of the variable _score via the use of ToString
+        _target.text = target.ToString("00");//appends the text value of the variable _target via the use of ToString
 
 
         if (score == target) //checks if the score is equal to target
         {
-            score = target;
-            WinText.SetActive(true); //then sets the win text on
+            score = target; //score is intialised as the target score 
+            WinText.SetActive(true); //then sets the win text to be active via the use of setActive()
             //spawner.SetActive(false);
             //spawner.GetComponent<Spawner>().enabled = false;
 
@@ -54,10 +55,10 @@ public class MovementRunner : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    { //method is called and actioned upon should the player enter the trigger collider
         if (other.gameObject.tag == "Item") //CHECKS IF THE BALL INTERACTS WITH THE CUBE THEN INCREMENTS THE SCORE BY 1
         {
-            score += 1;
+            score += 1;//incrementing score by 1 per cube collided with
         }
     }
 }
