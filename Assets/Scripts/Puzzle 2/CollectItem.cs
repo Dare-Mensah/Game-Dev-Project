1a793edcfef0387643743df26cc19c5464754554
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 12:51:28 +0000

    Update CollectItem.cs

--- Assets/Scripts/Puzzle 2/CollectItem.cs
+++ Assets/Scripts/Puzzle 2/CollectItem.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 using TMPro;
-using UnityEngine.UI;
+using UnityEngine.UI; //unity engine UI imported to allow for qualitative features to be deployed effectively
 public class CollectItem : MonoBehaviour
-{
+{ // the collectItem class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
     //track player score
     public int score;
     //value that player acheived
@@ -29,21 +30,21 @@ public class CollectItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
-        spawner.SetActive(false);
+        score = 0; //score is initialised as being 0 so to thus define a clear starting score
+        spawner.SetActive(false); //spawner is disable as the setactive() method is utilised to set it to a false state
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        _score.text = score.ToString("00");
-        _target.text = target.ToString("00");
+        _score.text = score.ToString("00"); //the score object is initialised to update the text property of the score object
+        _target.text = target.ToString("00");  //the target object is initialised to update the text property of the target object
 
         if(score == target)
-        {
-            score = target;
-            WinText.SetActive(true);
+        { //a condition based if statement is deployed in order to check whehter the players score is equal to the pre defined target score
+            score = target; //score is assigned to target through basic intialisation
+            WinText.SetActive(true); //the wintext variable is set to a true state via the use of setActive() method
             //spawner.SetActive(false);
             //spawner.GetComponent<Spawner>().enabled = false;
 
@@ -51,7 +52,7 @@ public class CollectItem : MonoBehaviour
         
         if(puzzleCam.activeInHierarchy == true) //CHECKS IF THE PUZZLE CAM IS ACTIVE THEN SETS THE SPAWNER TO TRUE
         {
-            spawner.SetActive(true);
+            spawner.SetActive(true); //setactive() method is used to active spawner variable that is resultantly set to being true
         }
 
         //if(Input.GetKey(KeyCode.Escape))
@@ -61,10 +62,10 @@ public class CollectItem : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    { //method called should player enter the trigger colliders
         if(other.gameObject.tag == "Item") //CHECKS IF THE BALL INTERACTS WITH THE CUBE THEN INCREMENTS THE SCORE BY 1
         {
-            score += 1;
+            score += 1; //incrementing score by 1 should the player make contact with a cube
         }
     }
 }
