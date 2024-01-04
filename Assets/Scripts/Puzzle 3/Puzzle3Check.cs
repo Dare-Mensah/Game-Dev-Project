edff3e31d238ec822fc6718bfe9f251018a975c1
Jayden Mehta-Patel <63342048+Jayden995050@users.noreply.github.com>
2024-01-04 17:20:13 +0000

    Update Puzzle3Check.cs

--- Assets/Scripts/Puzzle 3/Puzzle3Check.cs
+++ Assets/Scripts/Puzzle 3/Puzzle3Check.cs
@@ -1,18 +1,19 @@
-using System.Collections;
+using System.Collections; // use of statement ensures the system.collection is imported to be used for collections like arrays
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; //importing unity engine that will allow for unity based functions and features to be deployed successfully
 
 public class Puzzle3Check : MonoBehaviour
-{
+{ // the puzzle3check class that inherits from the monobehaviour class which thus enables for it to be assigned to the gameobjects and make use of unity features
+
     //camera specific to a puzzle
-    public GameObject puzzleCam;
+    public GameObject puzzleCam; //public reference made to game object puzzle cam
     // the player who is playing
-    public GameObject playerPuzzle3;
+    public GameObject playerPuzzle3; //public reference made to game object player puzzle 3
 
     // Start is called before the first frame update
     void Start()
     {
-        playerPuzzle3.SetActive(false);
+        playerPuzzle3.SetActive(false); //setActive() method sets the game object player puzzle 3 to false
     }
 
     // Update is called once per frame
@@ -20,7 +21,7 @@ public class Puzzle3Check : MonoBehaviour
     {
         if (puzzleCam.activeInHierarchy == true) //check if the puzzle cam is active then sets the puzzle player to active
         {
-            playerPuzzle3.SetActive(true);
+            playerPuzzle3.SetActive(true); //setActive() method sets the game object player puzzle 3 to true
         }
     }
     // this game is inspired by website https://gamedevacademy.org/unity-ski-mini-game-tutorial/
